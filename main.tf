642c3c318dd7f3a21585d086309dc041456d8756
Ronaldo Teixeira <ronaldoalvesteixeira@gmail.com>
2023-05-19 03:49:57 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    kubevirt = {
+      source = "kubevirt/kubevirt"
+      version = "0.0.1"
+    }
+  }
+}
+
 provider "kubevirt" {
 }
 provider "kubernetes" {
